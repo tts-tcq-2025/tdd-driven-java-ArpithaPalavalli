9512ac80b1eb4669793c2f6d04ce00dd6ae0a9a2
ArpithaPalavalli <ArpithaPalavalli.Venkatesulu@in.bosch.com>
2025-08-22 15:59:20 +0530

    Update StringCalculatorTest.java

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse; 
+import static org.junit.Assert.assertTrue;
 
 public class StringCalculatorTest {
 
